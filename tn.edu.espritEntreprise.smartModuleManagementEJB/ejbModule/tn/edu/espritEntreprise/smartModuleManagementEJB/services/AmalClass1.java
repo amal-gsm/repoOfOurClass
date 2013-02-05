cd8f001eec48fa3e2975ba20ce9a3f926aa0ef87
Amoula <Amoula@Amoula-PC>
2013-02-05 12:12:15 +0100

    Signed-off-by: Amoula <Amoula@Amoula-PC>

---
+++ tn.edu.espritEntreprise.smartModuleManagementEJB/ejbModule/tn/edu/espritEntreprise/smartModuleManagementEJB/services/AmalClass1.java
@@ -0,0 +1,13 @@
+package tn.edu.espritEntreprise.smartModuleManagementEJB.services;
+
+public class AmalClass1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
