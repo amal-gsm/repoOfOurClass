3d98ace9f781a54d1037ffa59097339d760f9e96
Amoula <Amoula@Amoula-PC>
2013-02-05 11:41:23 +0100

    this is amal class
    Signed-off-by: Amoula <Amoula@Amoula-PC>

---
+++ tn.edu.espritEntreprise.smartModuleManagementEJB/ejbModule/tn/edu/espritEntreprise/smartModuleManagementEJB/services/AmalClass.java
@@ -0,0 +1,13 @@
+package tn.edu.espritEntreprise.smartModuleManagementEJB.services;
+
+public class AmalClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
